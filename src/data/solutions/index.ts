12367eb2ad32a175ccdd5c48f20e0ee83f882097
Yaniv Raz <yanivmiz77@gmail.com>
2026-08-18 00:53:34 +0300

    feat: include right-angle solution registry

--- src/data/solutions/index.ts
+++ src/data/solutions/index.ts
@@ -7,6 +7,7 @@ import { SOLUTION_SPECS_46_50 } from './registry-46-50';
 import { SOLUTION_SPECS_51_58 } from './registry-51-58';
 import { SOLUTION_SPECS_59_64 } from './registry-59-64';
 import { SOLUTION_SPECS_65_70 } from './registry-65-70';
+import { SOLUTION_SPECS_RIGHT_ANGLE } from './registry-right-angle';
 import type { ResolvedSolutionPage } from './types';
 
 export type { ExerciseSolution, SolutionPageSpec, ResolvedSolutionPage } from './types';
@@ -20,6 +21,7 @@ export const SOLUTION_SPECS = [
   ...SOLUTION_SPECS_51_58,
   ...SOLUTION_SPECS_59_64,
   ...SOLUTION_SPECS_65_70,
+  ...SOLUTION_SPECS_RIGHT_ANGLE,
 ];
 
 /**
